1837523271a5af1be06206c049877a097e06522c
ilkin zeynalli <ilkin.zeynalli@bil.omu.edu.tr>
2021-07-05 17:54:33 +0400

    class binding

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -10,10 +10,27 @@ import { ProductRepository } from "./models/repository.model";
 export class ProductComponent{
     productRepository: ProductRepository;
     product: Product;
+    disabled: boolean = true;
 
     constructor(){
         this.productRepository = new ProductRepository();
         this.product = this.productRepository.getProductById(1);
+    }
+
+    getClasses(id:number){
+        let product = this.productRepository.getProductById(id);
+
+        return ((product.price ?? 0) < 2000 ? "bg-primary":"bg-secondary" + " m-2 p-2")
+    }
+
+    getClassesMap(id: number) :Object{
+        let product = this.productRepository.getProductById(id);
+
+        return {
+            "bg-primary": (product.price ?? 0) <= 2000,
+            "bg-secondary": (product.price ?? 0) > 2000,
+            "text-white": product.name == "Product 2"
+        };
     }
 
 }
